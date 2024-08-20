b6fb8972c1b883fc0a672f2fe28fe4c8cc4cbe1b
Xenrom <112312517+Xenrom@users.noreply.github.com>
2024-08-20 13:12:30 +0800

    added ui
    add buttons and some design

--- js.js
+++ js.js
@@ -15,94 +15,121 @@ function getComputerChoice(){
     return move;
 }
 
-function getHumanChoice(){
-    const humanMove = prompt("rock, paper, scissor: ");
-    return humanMove;
+function incrementHumanScore(){
+    humanScore.textContent = parseInt(humanScore.textContent) + 1;
 }
 
+function incrementComputerScore(){
+    computerScore.textContent = parseInt(computerScore.textContent) + 1;
+}
 
-
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;  
-    
-    
-    function playRound(humanChoice, computerChoice){
-        humanChoice = humanChoice.toLowerCase();
-        let announcement;
-        switch(humanChoice){
-            case "paper":
-                switch(computerChoice){
-                    case "paper":
-                        announcement = "Draw";
-                        break;
-                    case "rock":
-                        announcement = "You win! paper beats rock";
-                        humanScore++;
-                        break;
-                    case "scissor":
-                        announcement = "You lose! scissor beats paper";
-                        computerScore++;
-                        break;
-                    default:
-                        announcement = "something went wrong";
+ function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    let announcement;
+    switch(humanChoice){
+        case "paper":
+            switch(computerChoice){
+                case "paper":
+                    announcement = "Draw";
+                    break;
+                case "rock":
+                    announcement = "You win! paper beats rock";
+                    break;
+                case "scissor":
+                    announcement = "You lose! scissor beats paper";
+                    break;
+                default:
+                    announcement = "something went wrong";
+            }
+            break;
+        case "rock":
+            switch(computerChoice){
+                case "paper":
+                    announcement = "You lose! paper beats rock";
+                    break;
+                case "rock":
+                    announcement = "Draw";
+                    break;
+                case "scissor":
+                    announcement = "You win! rock beats scissor";
+                    break;
+                default:
+                    announcement = "something went wrong";
+            }
+            break;
+        case "scissor":
+            switch(computerChoice){
+                case "paper":
+                    announcement = "You win! scissor beats paper";
+                    break;
+                case "rock":
+                    announcement = "You lose! rock beats scissor";
+                    break;
+                case "scissor":
+                    announcement = "Draw";
+                    break;
+                default:
+                    announcement = "something went wrong";
                 }
-                break;
-            case "rock":
-                switch(computerChoice){
-                    case "paper":
-                        announcement = "You lose! paper beats rock";
-                        computerScore++;
-                        break;
-                    case "rock":
-                        announcement = "Draw";
-                        break;
-                    case "scissor":
-                        announcement = "You win! rock beats scissor";
-                        humanScore++;
-                        break;
-                    default:
-                        announcement = "something went wrong";
-                }
-                break;
-            case "scissor":
-                switch(computerChoice){
-                    case "paper":
-                        announcement = "You win! scissor beats paper";
-                        humanScore++;
-                        break;
-                    case "rock":
-                        announcement = "You lose! rock beats scissor";
-                        computerScore++;
-                        break;
-                    case "scissor":
-                        announcement = "Draw";
-                        break;
-                    default:
-                        announcement = "something went wrong";
-                    }
-                break;
-            default:
-                announcement = "please either put rock, paper or scissor";
+            break;
+        default:
+            announcement = "please either put rock, paper or scissor";
+    }
+    
+    console.log("your move " + humanChoice + "\n" + "computer move " + computerChoice + "\n");
+    alert(announcement);
+    const check = announcement.split("!");
+
+    if(check[0] === "You win"){
+        incrementHumanScore();
+    }
+    else if(check[0] === "You lose"){
+        incrementComputerScore();
+    }
+
+    const hScore = parseInt(humanScore.textContent);
+    const cScore = parseInt(computerScore.textContent);
+    if(hScore === 5 || cScore === 5){
+        if(hScore === 5){
+            alert("HUMANITY BEATS COMPUTER!!!");
+        }
+        else {
+            alert("COMPUTER BEATS HUMANITY!!");
         }
         
-        console.log("your move " + humanChoice + "\n" + "computer move " + computerChoice + "\n");
-        console.log(announcement);
+        
+        humanScore.textContent = 0;
+        computerScore.textContent = 0;
+        
     }
+}    
 
 
 
+const humanScore = document.querySelector("#humanScore");
+const computerScore = document.querySelector("#computerScore");
 
-    for(let i = 0; i < 5; i++){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    let announcement = "your score: " + humanScore + "\n" + "computer score: " + computerScore;
+const textDisplay = document.querySelector("span");
+const buttons = document.querySelectorAll("button");
 
-    if (humanScore > computerScore){
-        console.log(announcement + "\n" + "humanity won!");
-    }
-    else {
-        console.log(announcement + "\n" + "computer won!");
-    }
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(button.id === "playRound"){
+            if(textDisplay.textContent === "none"){
+                alert("please select a move first!")
+            }
+            else {
+                playRound(textDisplay.textContent, getComputerChoice())
+                textDisplay.textContent = "none";
+            }
+        }
+        else{
+            textDisplay.textContent = button.id;
+        }
+        }
+    )
 }
+)
+
+
+
